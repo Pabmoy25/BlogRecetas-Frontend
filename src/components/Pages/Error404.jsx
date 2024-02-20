daa17573fff4c65f97706feff8ea75e752345268
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-20 15:43:07 -0300

    Se agrega componente Error404 y se realiza su maquetado y las rutas correspondientes

--- src/components/Pages/Error404.jsx
+++ src/components/Pages/Error404.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { Container, Image, Button } from "react-bootstrap";
+import logoError from "../../../src/assets/error404.png";
+import { NavLink } from "react-router-dom";
+
 
 const Error404 = () => {
-  return (
-    <div>
-      <p>hola</p>
-    </div>
-  );
-};
+    return (
+        <>
+        <Container>
+          <NavLink end className="nav-link" to="/">
+            <div className="text-center d-flex align-items-center justify-content-center flex-column">
+              <Image src={logoError} className="img-fluid me-4 w-50" />
+              <Button variant="success" className="my-2 py-2">
+                Ir al Inicio
+              </Button>
+            </div>
+          </NavLink>
+        </Container>
+      </>
+    );
+  };
 
 export default Error404;
